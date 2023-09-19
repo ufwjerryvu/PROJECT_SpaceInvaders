0b6ff4377634a998e1275c8f54a8457cb10e5738
Jerry Vu <ufwjerryvu@gmail.com>
2023-09-19 12:39:47 +1000

    Added folders and files back

---
+++ README.md
@@ -0,0 +1,2 @@
+# Space Invaders
+## SOFT2201 Assignment 2
---
+++ source/src/main/java/invaders/App.java
@@ -0,0 +1,29 @@
+package invaders;
+
+import javafx.application.*;
+import javafx.stage.*;
+import invaders.engine.*;
+
+import java.util.*;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Map<String, String> params = getParameters().getNamed();
+
+        GameEngine model = new GameEngine("/resources/config.json");
+        GameWindow window = new GameWindow(model, 640, 400);
+        window.run();
+
+        stage.setTitle("Space Invaders");
+        stage.setScene(window.getScene());
+        stage.show();
+
+        window.run();
+    }
+}
