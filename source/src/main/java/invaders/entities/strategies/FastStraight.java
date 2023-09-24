dcccb53c837626f1be424c4492337561d0a71c74
Jerry Vu <ufwjerryvu@gmail.com>
2023-09-24 20:25:28 +1000

    Fixed the speed of fast straight

--- source/src/main/java/invaders/entities/strategies/FastStraight.java
+++ source/src/main/java/invaders/entities/strategies/FastStraight.java
@@ -2,7 +2,7 @@ package invaders.entities.strategies;
 
 public class FastStraight implements ProjectileStrategy {
     public double getStrategicSpeed(){
-        final double SPEED_IN_PIXELS = 2.5;
+        final double SPEED_IN_PIXELS = 3.0;
 
         return SPEED_IN_PIXELS;
     }
